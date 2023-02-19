ed8a8f5aabac61a118307aac41ab4bcd1ef42ace
Eloy Alonso <develoi89@Jarvis.local>
2023-02-19 12:16:44 +0100

    Added warnnings about full memory

--- Ex_01/src/PhoneBook.cpp
+++ Ex_01/src/PhoneBook.cpp
@@ -12,16 +12,40 @@ PhoneBook::~PhoneBook(void)
 
 void PhoneBook::addfc(void)
 {
-    if (this->i == 8)
-        this->i = 0;
+    std::string Answer;
     std::string FirstName;
     std::string LastName;
     std::string PhoneNumber;
     std::string DarkestSecret;
+    std::cout << "\x1b[36mPlease, type the first name:\x1b[37m" << std::endl;
     std::getline (std::cin, FirstName);
+    std::cout << "\x1b[36mNow, type the last name:\x1b[37m" << std::endl;
     std::getline (std::cin, LastName);
+    std::cout << "\x1b[36mHis phone:\x1b[37m" << std::endl;
     std::getline (std::cin, PhoneNumber);
+    std::cout << "\x1b[36mAnd finally his darkest secret...:\x1b[37m" << std::endl;
     std::getline (std::cin, DarkestSecret);
+    if (this->i == 8)
+    {
+        std::cout << "\033[1;31mWARNING, ACHTUNG,ATENTIE LA PERICOL, OH LA LA, CUIDADINI!!!, EH CUIDAO AHI!!!\x1b[37m" << std::endl;
+        std::cout << "\x1b[36mThe PhoneBook is full, the oldest contact will be deleted, are you sure?" << std::endl;
+        std::cout << "Type \033[1;32m(YES)\x1b[37m \x1b[36mor \033[1;32m(NO)\x1b[37m" << std::endl;
+        std::getline (std::cin, Answer);
+        while (this->i == 8)
+        {
+            if (Answer == "YES")
+            {
+                this->i = 0;
+            }
+            else if (Answer == "NO")
+                return;
+            else
+            {
+                std::cout << "\x1b[36mPlease, type only \033[1;32m(YES)\x1b[37m \x1b[36mor \033[1;32m(NO)\x1b[37m:" << std::endl;
+                std::getline (std::cin, Answer);
+            }
+        }
+    }
     this->contacts[this->i] = Contact(FirstName, LastName, PhoneNumber, DarkestSecret);
     this->i++;
     return;
@@ -41,10 +65,11 @@ void PhoneBook::srchfc(void)
     {
         for (i = 0; i <= 7; i++)
         {
-            std::cout << "\x1b[32m|\x1b[36m _" << i + 1 << " \x1b[36m\x1b[37m";
+            std::cout << "\x1b[32m|\x1b[36m _" << i + 1 << " \x1b[37m";
             this->contacts[i].Contact::ShowIt();
             std::cout << "\x1b[32m|------------------------------------------------|\x1b[37m" << std::endl;
         }
     }
+
     return;
 }
